d3567de74c8011148a81cb0f863ca28c9af51241
Jean Fabre <contact@fabrejean.net>
2017-07-24 10:51:24 +0300

    test action, will be removed once ecosystem browser works for this rep

---
+++ Assets/PlayMaker Custom Actions/Test/TestAction.cs
@@ -0,0 +1,19 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2017. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
